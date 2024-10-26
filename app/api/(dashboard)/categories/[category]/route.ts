cccf186a1b3ce2a25bfdef037f52ed5b948ceb4c
Victor okpaire <vixtorjason@gmail.com>
2024-10-26 23:48:29 +0100

    omo

--- app/api/(dashboard)/categories/[category]/route.ts
+++ app/api/(dashboard)/categories/[category]/route.ts
@@ -3,16 +3,69 @@
 import connect from "@/lib/db";
 import Category from "@/lib/modals/category";
 import User from "@/lib/modals/user";
+import { ObjectId } from "mongodb";
 import { Types } from "mongoose";
 import { NextResponse } from "next/server";
 
 export const PATCH = async (req: Request, context: { params: any }) => {
+  const categoryId = context.params.category;
   try {
     const body = await req.json();
     const { title } = body;
 
     const { searchParams } = new URL(req.url);
     const userId = searchParams.get("userId");
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ error: "Invalid or missing user ID" }),
+        { status: 400 }
+      );
+    }
+
+    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing categpryId" }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+
+    const category = await Category.findOne({ _id: categoryId, user: userId });
+
+    if (!category) {
+      return new NextResponse(
+        JSON.stringify({ message: "Category not found" }),
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      categoryId,
+      {
+        title,
+      },
+      { new: true }
+    );
+
+    return new NextResponse(
+      JSON.stringify({
+        message: "Category is updated",
+        category: updatedCategory,
+      }),
+      { status: 200 }
+    );
   } catch (err: any) {
     return new NextResponse("Error in updating category" + err.message, {
       status: 500,
