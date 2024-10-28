680abd43ad65be335b5851ab4c6547631da9cd8d
Victor okpaire <vixtorjason@gmail.com>
2024-10-28 21:08:59 +0100

    pushed updates

--- app/api/(dashboard)/blogs/[blog]/route.ts
+++ app/api/(dashboard)/blogs/[blog]/route.ts
@@ -7,3 +7,54 @@ import User from "@/lib/modals/user";
 import { Types } from "mongoose";
 import { NextResponse } from "next/server";
 import Blog from "@/lib/modals/blogs";
+
+export const GET = async (req: Request, context: { params: any }) => {
+  const blogId = context.params.blog;
+
+  try {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
+    const categoryId = searchParams.get("categoryId");
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing userId" }),
+        { status: 400 }
+      );
+    }
+
+    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing categoryId" }),
+        { status: 400 }
+      );
+    }
+
+    if (!blogId || !Types.ObjectId.isValid(blogId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing blogId" }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return new NextResponse(
+        JSON.stringify({ message: "Category not found" }),
+        { status: 404 }
+      );
+    }
+  } catch (err: any) {
+    return new NextResponse("Error in fetching a blog" + err.message, {
+      status: 500,
+    });
+  }
+};
