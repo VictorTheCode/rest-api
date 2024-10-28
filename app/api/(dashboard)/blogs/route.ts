58a16ca3d7017805204ec17f55de1fd036ea08d4
Victor okpaire <vixtorjason@gmail.com>
2024-10-28 18:52:40 +0100

    created blog

---
+++ app/api/(dashboard)/blogs/route.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-require-imports */
+import connect from "@/lib/db";
+import Category from "@/lib/modals/category";
+import User from "@/lib/modals/user";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+import Blog from "@/lib/modals/blogs";
+
+export const GET = async (req: Request) => {
+  try {
+  } catch (err: any) {}
+};
