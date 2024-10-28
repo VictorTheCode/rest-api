db88671f64904df5adc78464cbd6e27fee189078
Victor okpaire <vixtorjason@gmail.com>
2024-10-28 20:35:47 +0100

    lgtm

---
+++ app/api/(dashboard)/blogs/[blog]/route.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-require-imports */
+import connect from "@/lib/db";
+import Category from "@/lib/modals/category";
+import User from "@/lib/modals/user";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+import Blog from "@/lib/modals/blogs";
--- app/api/(dashboard)/blogs/route.ts
+++ app/api/(dashboard)/blogs/route.ts
@@ -74,6 +74,61 @@ export const POST = async (req: Request) => {
 
     const body = await req.json();
     const { title, description } = body;
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing userId" }),
+        { status: 400 }
+      );
+    }
+
+    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing categoryId" }),
+        { status: 400 }
+      );
+    }
+
+    if (!title || !description) {
+      return new NextResponse(
+        JSON.stringify({ message: "Title and description are required" }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return new NextResponse(
+        JSON.stringify({ message: "Category not found" }),
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const newBlog = new Blog({
+      title,
+      description,
+      user: new Types.ObjectId(userId),
+      category: new Types.ObjectId(categoryId),
+    });
+
+    await newBlog.save();
+    return new NextResponse(
+      JSON.stringify({ message: "Blog created", blog: newBlog }),
+      {
+        status: 201,
+      }
+    );
   } catch (err: any) {
     return new NextResponse("Error in creating blog" + err.message, {
       status: 500,
