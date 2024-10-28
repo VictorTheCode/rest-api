f2ba2d35d17b7c0bda649a89f4ed03d1495847cc
Victor okpaire <vixtorjason@gmail.com>
2024-10-28 20:06:48 +0100

    done with get intergration + working on the POST

--- app/api/(dashboard)/blogs/route.ts
+++ app/api/(dashboard)/blogs/route.ts
@@ -10,5 +10,73 @@ import Blog from "@/lib/modals/blogs";
 
 export const GET = async (req: Request) => {
   try {
-  } catch (err: any) {}
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
+    const categoryId = searchParams.get("categoryId");
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing userId" }),
+        { status: 400 }
+      );
+    }
+
+    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing categoryId" }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return new NextResponse(
+        JSON.stringify({ message: "Category not found" }),
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const filter: any = {
+      user: new Types.ObjectId(userId),
+      category: new Types.ObjectId(categoryId),
+    };
+
+    // TODO: Add some blog posts
+
+    const blogs = await Blog.find(filter);
+
+    return new NextResponse(JSON.stringify({ blogs }), {
+      status: 200,
+    });
+  } catch (err: any) {
+    return new NextResponse("Error in fetching blogs" + err.message, {
+      status: 500,
+    });
+  }
+};
+
+export const POST = async (req: Request) => {
+  try {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
+    const categoryId = searchParams.get("categoryId");
+
+    const body = await req.json();
+    const { title, description } = body;
+  } catch (err: any) {
+    return new NextResponse("Error in creating blog" + err.message, {
+      status: 500,
+    });
+  }
 };
